dded201cbbfcc880f643c29e7ddd12c75ba73c76
Vikas2201 <76476273+Vikas2201@users.noreply.github.com>
2021-04-12 22:48:19 +0530

    Add files via upload

---
+++ Problem 2/system_calls.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main()
+{
+    int pid, pid1, pid2 ;
+    pid = fork() ;
+    if(pid == -1)
+    {
+        printf("ERROR IN PROCESS CREATION \n");
+        exit(1);
+    }
+    if(pid != 0)
+    {  
+        pid1 = getpid();
+        printf("\n child process ID is %d\n", pid1);
+        
+        pid2 = getppid();
+        printf("\n parent of child process ID is %d\n", pid2);
+    }
+    return 0 ;
+}
